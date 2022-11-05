6c5668df14ad320ae4d82ca14faf2e06d4f88cc4
sam <89159750+samysam42@users.noreply.github.com>
2022-11-04 21:45:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let img = 1;
+const max = 5;
+const cb11 = document.getElementById("cb11");
+
+function nextImg(img){
+    fetch("img/" + img + ".png")
+        .then(resp => resp.blob())
+        .then(blob => {
+            const imageObjectURL = URL.createObjectURL(blob);
+            const proxImg = document.createElement("img");
+            proxImg.src = imageObjectURL;
+            cb11.appendChild(proxImg);
+        });
+}
+
+window.onload = ()=>{
+    if(img <=5){
+        for(let i=0; i<=3; i++){   
+            nextImg( img++ % (max+1));
+        }
+    }
+    
+}
+
+cb11.onscroll = ()=>{
+    nextImg( img++ % (max+1));
+}
